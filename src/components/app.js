52d5489768ab2c23a62ff01f378e3b0032817704
Ballred95 <blake.allred95@icloud.com>
2020-06-30 22:22:32 -0600

    basic api call to unsplash initiated in app.js

--- src/components/app.js
+++ src/components/app.js
@@ -1,11 +1,30 @@
 import React, { Component } from 'react';
+import axios from 'axios'
+
 
 export default class App extends Component {
+  constructor() {
+    super();
+
+    this.getSearchFoto = this.getSearchFoto.bind(this)
+  }
+  getSearchFoto () {
+    axios.get('https://api.unsplash.com/search/photos/?client_id=HN_kjPdQ-BoIKjcaX0ItpS0T0R4_CJb2C3ZycC6pWQI&query=america')
+  .then(response => {
+    // handle success
+    console.log(response);
+  })
+  .catch(function (error) {
+    // handle error
+    console.log(error);
+  })
+  }
+  
   render() {
+    this.getSearchFoto();
     return (
       <div className='app'>
-        <h1>DevCamp React Starter</h1>
-        <h2>React Redux Router</h2>
+        
       </div>
     );
   }
---
+++ src/components/unsplash.js
@@ -0,0 +1,4 @@
+import Unsplash from 'unsplash-js';
+
+const unsplash = new Unsplash({ accessKey: "{HN_kjPdQ-BoIKjcaX0ItpS0T0R4_CJb2C3ZycC6pWQI}" });
+
